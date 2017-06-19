c873b2049461118cf513dc05f5ed1b79c9907b25
Leonardo D'Antoni <leonardodantoni@Pablo-Feos-MacBook-Pro.local>
2017-06-19 14:23:01 -0300

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,9 +6,17 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  name1 = '';
+  name2 = '';
 
-  constructor(public navCtrl: NavController) {
+  get score(){
+    let letters = (this.name1 + this.name2).toLowerCase();
+    let sum = 0;
 
-  }
+    for(let i = 0; i<letters.length; i++){
+      sum += letters.charCodeAt(i);
+    }
 
+    return sum % 101;
+  };
 }
